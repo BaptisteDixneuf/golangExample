88eb68c0f260cd064a45461419161634c192d223
Dixneuf Baptiste <baptiste.dixneuf@gmail.com>
2017-07-18 17:47:56 +0200

    slice example

--- array-and-slices/array-and-slices.go
+++ array-and-slices/array-and-slices.go
@@ -2,22 +2,16 @@ package main
 
 import "fmt"
 
-const g_cap int = 5 // Total capacity of our list
-var g_groceries [g_cap]string
-var g_len int = 0 //total number of grocery items in our current array
+var g_groceries []string
 
 func add_groccery(a string) {
-	if g_len < g_cap {
-		g_groceries[g_len] = a
-		g_len++
-	} else {
-		fmt.Println("Too many items, now we are done  for!!")
-	}
+	fmt.Println("Capacity", cap(g_groceries))
+	g_groceries = append(g_groceries, a)
 }
 
 func list_groceries() {
 	fmt.Println("Grocery list is as follow:")
-	for i := 0; i < g_len; i++ {
+	for i := 0; i < len(g_groceries); i++ {
 		fmt.Println(g_groceries[i])
 	}
 }
@@ -25,6 +19,10 @@ func list_groceries() {
 func main() {
 	add_groccery("Bread")
 	add_groccery("Cucumbers")
+	add_groccery("Salt and Vinegar Potato Chips")
+	add_groccery("Salt and Vinegar Potato Chips")
+	add_groccery("Salt and Vinegar Potato Chips")
+	add_groccery("Salt and Vinegar Potato Chips")
 	add_groccery("Salt and Vinegar Potato Chips")
 	list_groceries()
 }
